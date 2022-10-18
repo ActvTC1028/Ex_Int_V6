10a97db1c30576e5057e69e7a0536feb98a9eef8
fabyuribe <78222449+fabyuribe@users.noreply.github.com>
2022-10-17 22:04:24 -0500

    Initial commit

---
+++ assignments/Ejercicio2/src/exercise.py
@@ -0,0 +1,17 @@
+# Función que despliega la matriz
+def despliega_matriz(matriz):
+    '''Función que despliega una lista anidada por elemento, generando un string 
+    con sus datos'''
+    for lista in matriz:
+        print(" ".join(lista))
+
+# Escribe aquí la función que hace falta
+
+
+
+def main():
+    #escribe tu código abajo de esta línea
+    pass
+
+if __name__=='__main__':
+    main()
